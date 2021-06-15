47df27bdcad484891668607992a861fe06475957
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-06-15 22:05:36 +0800

    <feat>[S2-2-W4-Memorizing-Game]: Create MVC modules

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const GAME_STATE = {
+  FirstCardAwaits: "FirstCardAwaits",
+  SecondCardAwaits: "SecondCardAwaits",
+  CardsMatchFailed: "CardsMatchFailed",
+  CardsMatched: "CardsMatched",
+  GameFinished: "GameFinished",
+};
+
 const Symbols = [
   "https://image.flaticon.com/icons/svg/105/105223.svg", // 黑桃
   "https://image.flaticon.com/icons/svg/105/105220.svg", // 愛心
@@ -6,15 +14,18 @@ const Symbols = [
 ];
 const view = {
   getCardElement(index) {
+    return `<div data-index="${index}" class="card back"></div>`;
+  },
+  getCardContent(index) {
     const number = this.transformNumber((index % 13) + 1);
     const symbol = Symbols[Math.floor(index / 13)];
     return `
-        <div class="card">
           <p>${number}</p>
           <img src="${symbol}" />
           <p>${number}</p>
-        </div>`;
+        `;
   },
+
   transformNumber(number) {
     switch (number) {
       case 1:
@@ -29,23 +40,51 @@ const view = {
         return number;
     }
   },
-  displayCards() {
+  displayCards(indexes) {
     const rootElement = document.querySelector("#cards");
-    rootElement.innerHTML = utility.getRandomNumberArray(52)
+    rootElement.innerHTML = indexes
       .map((index) => this.getCardElement(index))
       .join("");
+  },
+  flipCard(card) {
+    if (card.classList.contains("back")) {
+      // 回傳正面
+      card.classList.remove("back");
+      card.innerHTML = this.getCardContent(Number(card.dataset.index));
+      return;
+    }
+    // 回傳背面
+    card.classList.add("back");
+    card.innerHTML = null;
+  },
+};
+const model = {
+  revealedCards: [],
+};
+const controller = {
+  currentState: GAME_STATE.FirstCardAwaits,
+  generateCards() {
+    view.displayCards(utility.getRandomNumberArray(52));
   },
 };
 
 const utility = {
-    getRandomNumberArray (count) {
-      const number = Array.from(Array(count).keys())
-      for (let index = number.length - 1; index > 0; index--) {
-        let randomIndex = Math.floor(Math.random() * (index + 1))
-          ;[number[index], number[randomIndex]] = [number[randomIndex], number[index]]
-      }
-      return number
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys());
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1));
+      [number[index], number[randomIndex]] = [
+        number[randomIndex],
+        number[index],
+      ];
     }
-  }
+    return number;
+  },
+};
 
-view.displayCards();
+controller.generateCards();
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", (event) => {
+    view.flipCard(card);
+  });
+});
