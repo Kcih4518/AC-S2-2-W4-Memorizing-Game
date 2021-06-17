bf59b90fad0eeca7abe29f6dde7507bc241c6ed6
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-06-17 11:56:06 +0800

    <feat>[S2-2-W4-Memorizing-Game]: Pairing success/failure

--- index.js
+++ index.js
@@ -57,9 +57,18 @@ const view = {
     card.classList.add("back");
     card.innerHTML = null;
   },
+  pairCard(card) {
+    card.classList.add("paired");
+  },
 };
 const model = {
   revealedCards: [],
+  isRevealedCardsMatched() {
+    return (
+      this.revealedCards[0].dataset.index % 13 ===
+      this.revealedCards[1].dataset.index % 13
+    );
+  },
 };
 const controller = {
   currentState: GAME_STATE.FirstCardAwaits,
@@ -80,6 +89,23 @@ const controller = {
         view.flipCard(card);
         model.revealedCards.push(card);
         // 判斷配對是否成功
+        if (model.isRevealedCardsMatched()) {
+          // 配對成功
+          this.currentState = GAME_STATE.CardsMatched;
+          view.pairCard(model.revealedCards[0]);
+          view.pairCard(model.revealedCards[1]);
+          model.revealedCards = [];
+          this.currentState = GAME_STATE.FirstCardAwaits;
+        } else {
+          // 配對失敗
+          this.currentState = GAME_STATE.CardsMatchFailed;
+          setTimeout(() => {
+            view.flipCard(model.revealedCards[0]);
+            view.flipCard(model.revealedCards[1]);
+            model.revealedCards = [];
+            this.currentState = GAME_STATE.FirstCardAwaits;
+          }, 1000);
+        }
         break;
     }
     console.log("this.currentState", this.currentState);
